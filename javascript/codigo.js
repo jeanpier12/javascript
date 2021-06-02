236a73b4f8a207fc63bb2a975e722d41304c6591
jeanpier12 <81602541+jeanpier12@users.noreply.github.com>
2021-06-01 21:36:09 -0500

    embiar

---
+++ javascript/codigo.js
@@ -0,0 +1,18 @@
+alert("hola mundo xd");
+document.write("lo que tu quieras");
+
+let variable=prompt("dime tu nombre");
+alert("hola perro " + variable);
+let numero1=10;
+let numero2=5;
+alert(numero1+numero2);
+let numero5="10";
+console.log( typeof numero5);
+ let x="valor";
+let y=5;
+let s=8;
+let total= ""+ x + ""+ y+s;
+document.write(total);
+
+ let codigo=`<h1> hola perro </h1>`;
+ document.write(codigo);
---
+++ javascript/nuevo.js
@@ -0,0 +1,20 @@
+
+let roberto= 1.5;
+let pedro=1.7;
+let cofla=3;
+ 
+let agua=0.6;
+let crema=1;
+let heladix=1.6;
+let heladovich=1.7;
+let helardo=1.8;
+let confites=2.9;
+let pote=2.9;
+
+let guardar=prompt("ingrese el monto que tiene");
+
+
+
+if (guardar<=heladix){
+    alert("el helado mas caro que puedes comprar es heladix y su precio es de "+heladix );
+}
